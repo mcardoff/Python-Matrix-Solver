0bcad73af140fe8118208d852def0c496e08fe19
Michael Cardiff <mcardiff0321@gmail.com>
2022-05-20 00:08:36 -0500

    add hamiltonian computation

---
+++ generatehamiltonian.py
@@ -0,0 +1,22 @@
+from infinitesquarewell import InfiniteSquareWell
+import numpy as np
+
+def mel(psil, V, psir, ISW):
+    assert(isinstance(ISW, InfiniteSquareWell))
+    el = sum(l*v*r for (l,v,r) in zip(psil,V,psir))
+    return ISW.well_width * el / ISW.steps
+
+def compute_hamiltonian(V, ISW):
+    assert(isinstance(ISW, InfiniteSquareWell))
+    hamiltonian = []
+    for i in range(ISW.energy_eigenvals):
+        row = [] # one row of the hamiltonian matrix
+        for j in range(ISW.energy_eigenvals):
+            psil, psir = ISW.basis_funcs[i], ISW.basis_funcs[j]
+            el = mel(psil, V, psir)
+            if i == j: # diagonal elements get kinetic
+                el -= ISW.energy_eigenvals[i]
+            row.append(el)
+        hamiltonian.append(row)
+
+    return hamiltonian
