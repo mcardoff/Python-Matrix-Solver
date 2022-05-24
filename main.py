dd9bdc9c9f2dde46d09b103354a2aea166683dc6
Michael Cardiff <mcardiff0321@gmail.com>
2022-05-24 14:56:28 -0500

    basic function is 'working'

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+# libraries
 import numpy as np
 import numpy.linalg as la
 import matplotlib.pyplot as plt
-import random
+# files
 from potentials import PotentialType
 from infinitesquarewell import InfiniteSquareWell
 from generatehamiltonian import *
@@ -12,11 +13,33 @@ def main():
     ISW = InfiniteSquareWell()
     # choose potential
     potential = PotentialType.linear
-    V = potential.get_potential(ISW,1.0)
+    V = potential.get_potential(ISW,100.0)
     # compute hamiltonian matrix from the potential 
     H = compute_hamiltonian(V, ISW)
     # diagonalize hamiltonian, getting eigenvals and eigenvecs
-    vecs, vals = la.eig(H)
+    vals, vecs = la.eig(H)
+    # new functions are the eigenvectors time the eigenfunctions of ISW
+    newfuncs = []
+    for vec in vecs:
+        lin_combination = []
+        first_run = True
+        for (i,val) in enumerate(vec):
+            if first_run:
+                # need to build up list
+                for (j,basis_val) in enumerate(ISW.basis_funcs[i]):
+                    lin_combination.append(vec[i] * ISW.basis_funcs[i][j])
+                first_run = False
+            else:
+                for (j,basis_val) in enumerate(ISW.basis_funcs[i]):
+                    lin_combination[j] += vec[i] * basis_val
+
+        # one linear combination is built
+        newfuncs.append(lin_combination)
+
+    # for func in newfuncs:
+    plt.plot(ISW.xvals, newfuncs[0])
+
+    plt.show()
 
 if __name__ == "__main__":
     main()
