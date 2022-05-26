a371480b80bff4fe32997322239c53a7e85eeac1
Michael Cardiff <mcardiff0321@gmail.com>
2022-05-26 01:14:30 -0500

    add tkinter backend

--- main.py
+++ main.py
@@ -2,13 +2,21 @@
 import numpy as np
 import numpy.linalg as la
 import matplotlib.pyplot as plt
+import tkinter
+
+from matplotlib.backends.backend_tkagg import(FigureCanvasTkAgg,
+                                              NavigationToolbar2Tk)
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+
 # files
 from potentials import PotentialType
 from infinitesquarewell import InfiniteSquareWell
 from generatehamiltonian import *
+from window_events import *
 
-def main():
-    # get infinite square well basis
+def solve_problem():
+        # get infinite square well basis
     ISW = InfiniteSquareWell()
     # choose potential
     potential = PotentialType.linear
@@ -26,13 +34,65 @@ def main():
         newfuncs.append(lin_combination)
 
     x = ISW.xvals
-    plt.xlim(0, 1)
-    plt.ylim(-2, 2)
-    plt.plot(x,V,'black')
-    for func in newfuncs:
-        plt.plot(x, func)
 
-    plt.show()
+    # plt.xlim(0, 1)
+    # plt.ylim(-2, 2)
+    # plt.plot(x,V,'black')
+    # for func in newfuncs:
+        # plt.plot(x, func)
 
+    # plt.show()
+    
+    return (x,newfuncs)
+
+def main():
+    global canvas,toolbar,selector
+    root = tkinter.Tk()
+    root.wm_title("1-D Schrodinger")
+
+    selector = 0
+
+    x,funcs = solve_problem()
+
+    fig = Figure(figsize=(5, 4), dpi=100)
+    
+    subfig = fig.add_subplot(111)
+    subfig.set_xlim(0,max(x))
+    subfig.set_ylim(-2,2)
+
+    canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
+    canvas.draw()
+    canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+    toolbar = NavigationToolbar2Tk(canvas, root)
+    toolbar.update()
+    canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+    button = tkinter.Button(master=root, text="Quit", command=_quit)
+    button.pack(side=tkinter.BOTTOM)
+
+    selector_button = tkinter.Button(master=root, text="Next Plot",
+            command=lambda: inc_selector(subfig,x,funcs))
+    selector_button.pack(side=tkinter.BOTTOM)
+    
+    tkinter.mainloop()
+
+def _quit():
+    root.quit()     # stops mainloop
+    root.destroy()  # this is necessary on Windows to prevent
+                    # Fatal Python Error: PyEval_RestoreThread: NULL tstate
+
+def inc_selector(subfig,x,funcs):
+    global canvas,selector
+    subfig.clear()
+    subfig.set_xlim(0,max(x))
+    subfig.set_ylim(-2,2)
+    subfig.plot(x,funcs[selector])
+    if selector < len(funcs)-1:
+        selector += 1
+    else:
+        selector = 0
+    canvas.draw()
+    
 if __name__ == "__main__":
     main()
