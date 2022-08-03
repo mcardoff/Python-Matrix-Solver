1bf00b67cd4976a20a9edfa03e2cf063a77fafd5
Michael Cardiff <mcardiff0321@gmail.com>
2022-08-03 15:59:48 -0500

    Comments in generatehamiltonian

--- generatehamiltonian.py
+++ generatehamiltonian.py
@@ -2,11 +2,13 @@ from infinitesquarewell import InfiniteSquareWell
 import numpy as np
 
 def mel(psil, V, psir, ISW):
+    """ Compute matrix element using average value theorem """
     assert(isinstance(ISW, InfiniteSquareWell))
-    el = sum(l*v*r for (l,v,r) in zip(psil,V,psir))
-    return float(ISW.well_width * el / ISW.steps)
+    el = sum(l*v*r for (l,v,r) in zip(psil,V,psir)) # discrete inner product
+    return float(ISW.well_width * el / ISW.steps) # readjust for avg val thm
 
 def compute_hamiltonian(V, ISW):
+    """ Compute discretized hamiltonian """
     assert(isinstance(ISW, InfiniteSquareWell))
     hamiltonian = []
     for i in range(ISW.energy_eigenvals):
