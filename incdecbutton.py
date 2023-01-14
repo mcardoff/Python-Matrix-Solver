ebd2f04bd4b1ff33599c9b86459b4d6509d2615a
michael <mcardiff0321@gmail.com>
2023-01-14 18:44:42 -0500

    Add potential so plotting is easier

--- incdecbutton.py
+++ incdecbutton.py
@@ -10,15 +10,15 @@ from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
 
 class IncDecButton:
-    def __init__(self, subfig, canvas, x, funcs):
+    def __init__(self, subfig, canvas, x, funcs, V):
         self.selector = 0 # which one do we show
         self.subfig = subfig # where to we put it
         self.canvas = canvas # ditto
         self.x = x # same x values for all the functions
         self.funcs = funcs # library of functions
+        self.potential = V # potential 
         self.energy_eigenvals = len(funcs) # determined in main
-        self.calc_max_y() 
-        self.calc_max_x() # determined by well width
+        self.calc_maxes() # x and y limits
 
     # plot the function func, clearing previous plot and resetting limits
     def replot(self,func):
@@ -50,7 +50,7 @@ class IncDecButton:
     def plot_potential(self,V):
         self.subfig.set_xlim(0,self.x_max)
         self.subfig.set_ylim(-self.max_val,self.max_val)
-        self.subfig.plot(self.x_max,V)
+        self.subfig.plot(self.x,V)
         self.canvas.draw()
 
     # show first plot in the sequence, ignore value of selector
@@ -58,15 +58,13 @@ class IncDecButton:
         self.replot(self.funcs[0])
 
     # calculate and recalculate maxes
-    def calc_max_y(self):
+    def calc_maxes(self):
         self.max_val = max(map(max,map(abs,self.funcs)))+0.1 # y limits
+        self.x_max = max(self.x) # well width determines x limit
 
-    def calc_max_x(self):
-        self.x_max = max(self.x)
-
-    def update_vals(self, x, funcs):
+    def update_vals(self, x, funcs, V):
         self.x = x
         self.funcs = funcs
-        self.calc_max_x()
-        self.calc_max_y()
+        self.potential = V
+        self.calc_maxes()
         self.init_plot()
