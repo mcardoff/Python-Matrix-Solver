9814d8c3f4097d194e4daaa967860984087ff4d1
Michael Cardiff <mcardiff0321@gmail.com>
2023-01-15 12:51:28 -0600

    Lint generatehamiltonian

--- generatehamiltonian.py
+++ generatehamiltonian.py
@@ -1,22 +1,26 @@
+"""Generate the hamiltonian given a basis and potential."""
 from infinitesquarewell import InfiniteSquareWell
-import numpy as np
+
 
 def mel(psil, V, psir, ISW):
-    """ Compute matrix element using average value theorem """
+    """Compute matrix element using average value theorem."""
     assert(isinstance(ISW, InfiniteSquareWell))
-    el = sum(l*v*r for (l,v,r) in zip(psil,V,psir)) # discrete inner product: < left | V | right >
-    return float(ISW.well_width * el / ISW.steps) # readjust for avg val thm
+    # discrete inner product: < left | V | right >
+    el = sum(l*v*r for (l, v, r) in zip(psil, V, psir))
+    # readjust for avg val thm
+    return float(ISW.well_width * el / ISW.steps)
+
 
 def compute_hamiltonian(V, ISW):
-    """ Compute discretized hamiltonian """
+    """Compute discretized hamiltonian."""
     assert(isinstance(ISW, InfiniteSquareWell))
     hamiltonian = []
     for i in range(ISW.energy_eigenvals):
-        row = [] # one row of the hamiltonian matrix
+        row = []  # one row of the hamiltonian matrix
         for j in range(ISW.energy_eigenvals):
             psil, psir = ISW.basis_funcs[i], ISW.basis_funcs[j]
             el = mel(psil, V, psir, ISW)
-            if i == j: # diagonal elements get kinetic
+            if i == j:  # diagonal elements get kinetic
                 el += ISW.eigenvals[i]
             row.append(el)
             el = 0.0
